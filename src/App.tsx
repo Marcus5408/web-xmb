7234579790222a8477d3e9ed586b382cee661d69
Issac Liu <issac@matchatea.dev>
2025-07-30 07:16:00 -0400

    lint: format

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,41 @@
-import './App.css'
+import "./App.css";
 // import { ModeToggle } from './components/mode-toggle'
-import { XMBContainer, XMBPrimaryMenu, XMBMenuCategory, XMBMenuItem } from './components/web-xmb'
+import {
+  XMBContainer,
+  XMBPrimaryMenu,
+  XMBMenuCategory,
+  XMBMenuItem,
+} from "./components/web-xmb";
 
 function App() {
-
   return (
     <>
       {/* <ModeToggle /> */}
       {/* XMB Example */}
-      <XMBContainer className='w-100'>
+      <XMBContainer className="w-100">
         <XMBPrimaryMenu>
           <XMBMenuCategory title="Games">
-            {
-              Array.from({ length: 5 }, (_, i) => (
-                <XMBMenuItem index={i} title={`Game ${i + 1}`} icon={`Gamepad2`} />
-              ))
-            }
+            {Array.from({ length: 5 }, (_, i) => (
+              <XMBMenuItem
+                index={i}
+                title={`Game ${i + 1}`}
+                icon={`Gamepad2`}
+              />
+            ))}
           </XMBMenuCategory>
           <XMBMenuCategory title="Videos">
-                        {
-              Array.from({ length: 5 }, (_, i) => (
-                <XMBMenuItem index={i} title={`Video ${i + 1}`} icon={`square-play`} />
-              ))
-            }
+            {Array.from({ length: 5 }, (_, i) => (
+              <XMBMenuItem
+                index={i}
+                title={`Video ${i + 1}`}
+                icon={`square-play`}
+              />
+            ))}
           </XMBMenuCategory>
         </XMBPrimaryMenu>
       </XMBContainer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
